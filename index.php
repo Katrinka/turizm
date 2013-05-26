c3a794a949e41e9e0fd20785b20ba42948e00c02
Katrinka <katrinkaserdyuk@mail.ru>
2013-05-26 22:32:38 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>McDonalds</title>
+</head>
+<body>
+<a href="serdyuk.htm"> Заказать</a><br>
+<a href="mac.php"> просмотреть заказ </a>
+</body>
+</html>
